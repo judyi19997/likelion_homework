02d16224b3598e978496796c2a4c0d4dcbf617a2
judyi19997 <judyi19997@kookmin.ac.kr>
2020-05-11 19:04:28 +0900

    css 수정

--- wordcount/views.py
+++ wordcount/views.py
@@ -7,17 +7,24 @@ def home(request):
 
 def result(request):
     text = request.GET['fullText']
-    words_list = text.split()
-    # for word in words_list:
-    #     if word in '(':
-    #         word = word[1:]
-    #         print(word)
+    words_list = text.replace("(", "").replace(")","").split()
     length = len(words_list)
     word_dict = {}
+    number = 0
     for word in words_list:
+        word = word.lower()
         if word in word_dict:
             word_dict[word] +=1
         else:
             word_dict[word] = 1
-    return render(request, 'result.html', {'text':text , 'words':words_list, 'word_dict':word_dict.items(), 'length':length})
+
+    word_dict = sorted(word_dict.items(), reverse=True, key=lambda item: item[1])
+    final_length = range(len(word_dict))
+    # print(word_dict)
+    # print(word_dict.keys())
+    # key_list = list(word_dict.keys())
+    # value_list = list(word_dict.values())
+    # print(key_list)
+    return render(request, 'result.html', {'text':text , 'words':words_list, 'length' : length,
+    'word_dict': word_dict, 'final_length':final_length})
 
